44766a67a6b61dce0fef1dbc514647656bb270d9
Ashley McKellar <ashley.l.mckellar@gmail.com>
2023-11-24 20:30:28 -0800

    still messing with the proxy

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api', // change this to match your API endpoint//
+    '/api', // change this to match your API endpoint
     createProxyMiddleware({
       target: process.env.REACT_APP_API_URL || 'http://localhost:8100',
       changeOrigin: true,
