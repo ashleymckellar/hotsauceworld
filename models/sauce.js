535d43363a5dd1a4d07a653dcb74a5734901e0a7
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-06-03 21:59:19 -0700

    updated

--- models/sauce.js
+++ models/sauce.js
@@ -24,7 +24,7 @@ const sauceSchema = new Schema({
         type: String,
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "User",
         required: true,
     },
