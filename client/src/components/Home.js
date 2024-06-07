b1a710f523481db15e19c669568369d7d08a4e81
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-06-07 13:29:36 -0700

    updated image responsiveness

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -13,19 +13,23 @@ function Home() {
             className="welcome mx-auto row g-3 p-5"
             style={{ backgroundImage: { chilipeppers } }}
         >
-           
-                <h1 className="world">Hot Sauce World</h1>
-                <br></br>
-                <h2 className="ready">
-                    Ready to spice things up? Sign up or log in below to view
-                    our world of hot sauces
-                </h2>
-                {/* <img src={chiliPepper} className='two-red-chili-peppers'/> */}
-                <div>
-                    <Auth />
-                    <img src={chilipeppers} alt="chili peppers"/>
+            <h1 className="world">Hot Sauce World</h1>
+            <br></br>
+            <h2 className="ready">
+                Ready to spice things up? Sign up or log in below to view our
+                world of hot sauces
+            </h2>
+            {/* <img src={chiliPepper} className='two-red-chili-peppers'/> */}
+            <div>
+                <Auth />
+                <div className="responsive-image-container">
+                    <img
+                        src={chilipeppers}
+                        alt="chili peppers"
+                        className="chili-pepper"
+                    />
                 </div>
-            
+            </div>
         </div>
     );
 };
