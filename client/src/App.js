e019daeed8e61f9054a3e8e82bfadc75cede4978
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-06-12 10:28:03 -0700

    removed navigate from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,90 +1,95 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap';
-import { UserContext } from "./context/UserProvider";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { UserContext } from './context/UserProvider';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './components/Home.js';
-import Header from "./components/Header";
-import SauceListContainer from "./components/SauceListContainer.js"
-import Search from "./components/Search.js"
-import Profile from "./components/Profile";
-import Details from "./components/Details"
+import Header from './components/Header';
+import SauceListContainer from './components/SauceListContainer.js';
+import Search from './components/Search.js';
+import Profile from './components/Profile';
+import Details from './components/Details';
 
 function App() {
-  const { token, logout } = useContext(UserContext)
-  const [newSauce] = useState({})
+    const { token, logout } = useContext(UserContext);
+    const [newSauce] = useState({});
 
+    useEffect(() => {
+        console.log(newSauce);
+    }, [newSauce]);
 
+    return (
+        <div>
+            <Router>
+                <div className="bg-white">
+                    {token && <Header />}
 
-useEffect(()=> {
-  console.log(newSauce);
-}, [newSauce]);
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                token ? <Navigate to="/profile" /> : <Home />
+                            }
+                        />
 
-  return (
-    
-    <div>
-      <Router>
-      <div className="bg-white">
-      {token && <Header />} 
-      
-    
-        <Routes>
-          <Route path="/" 
-            element={token? <Navigate to="/profile" /> : <Home />}
-            />
+                        <Route
+                            path="/saucelist"
+                            element={
+                                <ProtectedRoute token={token} redirectTo="/">
+                                    <SauceListContainer />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/profile"
+                            element={
+                                <ProtectedRoute token={token} redirectTo="/">
+                                    <Profile />
+                                </ProtectedRoute>
+                            }
+                        />
 
-            
-          <Route 
-            path="/saucelist" 
-            element={<ProtectedRoute token={token} redirectTo="/">
-              <SauceListContainer   />
-              </ProtectedRoute>}
-              />
-          <Route 
-            path="/profile" 
-            element={<ProtectedRoute token={token} redirectTo="/">
-              <Profile  />
-            </ProtectedRoute>}
-          />
-         
-          <Route 
-            path="/search" 
-            element={<ProtectedRoute token={token} redirectTo="/">
-            <Search  />
-            </ProtectedRoute>}
-            />
-          <Route 
-            path="/saucelist/details/:sauceId" 
-            element={<ProtectedRoute token={token} redirectTo="/">
-              <Details />
-              </ProtectedRoute>}
-            />
+                        <Route
+                            path="/search"
+                            element={
+                                <ProtectedRoute token={token} redirectTo="/">
+                                    <Search />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/saucelist/details/:sauceId"
+                            element={
+                                <ProtectedRoute token={token} redirectTo="/">
+                                    <Details />
+                                </ProtectedRoute>
+                            }
+                        />
 
-<Route 
-            path="/profile/details/:sauceId" 
-            element={<ProtectedRoute token={token} redirectTo="/">
-              <Details />
-              </ProtectedRoute>}
-            />
-             <Route 
-            path="/search/details/:sauceId" 
-            element={<ProtectedRoute token={token} redirectTo="/">
-              <Details />
-              </ProtectedRoute>}
-            />
-          
-        </Routes>
-        
-      </div>
-
-      </Router>
-    
-
-    </div>
-  );
-  }
+                        <Route
+                            path="/profile/details/:sauceId"
+                            element={
+                                <ProtectedRoute token={token} redirectTo="/">
+                                    <Details />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/search/details/:sauceId"
+                            element={
+                                <ProtectedRoute token={token} redirectTo="/">
+                                    <Details />
+                                </ProtectedRoute>
+                            }
+                        />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
+}
 
 export default App;
