30a9d6a7c1aa2fe4746132d7c1477128f5f28456
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-07-15 11:36:34 -0700

    updated random sauce component

--- client/src/components/RandomSauce.js
+++ client/src/components/RandomSauce.js
@@ -19,36 +19,41 @@ export default function RandomSauce(props) {
     console.log(theRandomSauce);
 
     return (
-        <div className='bg-white py-3 text-center'>
-            <button className='btn btn-danger' onClick={() => getRandomSauce()}>Get Random Sauce!</button>
+        <div className="bg-white py-3 text-center">
+            <button
+                className="btn btn-danger my-3"
+                onClick={() => getRandomSauce()}
+            >
+                Get Random Sauce!
+            </button>
             <div className="details-div">
                 <div className="bg-white py-3 text-center">
-                    <h1>{theRandomSauce.name}</h1>
-                 
+                    <h1 className="py-3">{theRandomSauce.name}</h1>
+
                     {theRandomSauce.imageUrl &&
                     theRandomSauce.imageUrl !== null ? (
                         <img
                             src={theRandomSauce.imageUrl}
-                           
                             alt="hot sauce"
                             className="details-pic"
                         />
                     ) : (
                         <img
                             src={pepper}
-                            className="pepper submitted-pic card-img-top cardsize"
                             alt="pepper"
+                            className="details-pic"
                         />
                     )}
-                       <h5>
-                        {' '}
-                        Heat Rating (Scoville Units): {' '}
-                        {theRandomSauce.heatRating}
-                    </h5>
-                    <h5>Origin: {theRandomSauce.origin}</h5>
-                    <h5>Description: {theRandomSauce.description}</h5>
-                    <h5>ingredients: {theRandomSauce.ingredients}</h5>
-
+                    <div className="px-20">
+                        <h5 className="py-3 my-3">
+                            {' '}
+                            Heat Rating (Scoville Units):{' '}
+                            {theRandomSauce.heatRating}
+                        </h5>
+                        <h5>Origin: {theRandomSauce.origin}</h5>
+                        <h5>Description: {theRandomSauce.description}</h5>
+                        <h5>Ingredients: {theRandomSauce.ingredients}</h5>
+                    </div>
                 </div>
             </div>
         </div>
