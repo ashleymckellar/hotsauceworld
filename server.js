f19577d305c60fe151038f0e8e5daca0aee14b26
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-09-01 22:11:15 -0700

    added server code

--- server.js
+++ server.js
@@ -33,8 +33,12 @@ connectToDb()
         }
       });
       
-      // Serve static files from the React app
+     
       app.use(express.static(path.join(__dirname, 'client/build')));
+
+      app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+      });
 
 
 app.get('/logout', (req, res) => {
